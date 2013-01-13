1ba991e5b9effa0cdf186d7718aa4f591efa769f
Alan Burke <alan@cycletraffic.com>
2013-01-13 16:34:51 +0000

    Generate new build

---
+++ dist/scripts/586f752f.maps.js
@@ -0,0 +1,66 @@
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/da41d29fd31b4e8496d6f56f9dc70cbb/{styleId}/256/{z}/{x}/{y}.png',
+    cloudmadeAttribution = 'Map data &copy; 2013 OpenStreetMap contributors, Imagery &copy; 2013 CloudMade';
+
+var minimal   = L.tileLayer(cloudmadeUrl, {styleId: 47926, attribution: cloudmadeAttribution}),
+    midnight  = L.tileLayer(cloudmadeUrl, {styleId: 999,   attribution: cloudmadeAttribution}),
+
+var routes = L.geoJson(routes, {
+
+  style: function (feature) {
+    return feature.properties && feature.properties.style;
+  },
+
+  onEachFeature: onEachFeature,
+
+});
+
+var hqs = L.geoJson(hqs, {
+
+  style: function (feature) {
+    return feature.properties && feature.properties.style;
+  },
+
+  onEachFeature: onEachFeature,
+
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, {
+      radius: 5,
+      fillColor: "#ff7800",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    });
+  }
+
+});
+
+
+var map = L.map('main-map', {
+    center: [53.357, -8.83],
+    zoom: 9,
+    maxZoom: 14,
+    layers: [minimal, routes, hqs]
+
+});
+
+var baseMaps = {
+    "Day": minimal,
+    "Night": midnight
+};
+
+var overlayMaps = {
+    "Routes": routes,
+    "HQs": hqs
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.popupContent) {
+    popupContent = feature.properties.popupContent;
+  }
+
+  layer.bindPopup(popupContent);
+}
