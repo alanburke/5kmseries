2bbf5c8a40b3e2cac65cca37d2a18539ebdb735f
Alan Burke <alan@cycletraffic.com>
2013-01-24 21:40:54 +0000

    Move some results functions to a generic results js file

--- app/scripts/results-2012.js
+++ app/scripts/results-2012.js
@@ -76,27 +76,3 @@ $(document).ready(function(){
   });
 
 });
-
-function fnShowHide(iCol) {
-    /* Get the DataTables object again - this is not a recreation, just a get of the object */
-    var oTable = $('#results').dataTable();
-
-    var bVis = oTable.fnSettings().aoColumns[iCol].bVisible;
-    oTable.fnSetColumnVis( iCol, bVis ? false : true );
-}
-
-function fnShow(iCol) {
-    /* Get the DataTables object again - this is not a recreation, just a get of the object */
-    var oTable = $('#results').dataTable();
-
-    var bVis = oTable.fnSettings().aoColumns[iCol].bVisible;
-    oTable.fnSetColumnVis( iCol, true );
-}
-
-function fnHide(iCol) {
-    /* Get the DataTables object again - this is not a recreation, just a get of the object */
-    var oTable = $('#results').dataTable();
-
-    var bVis = oTable.fnSettings().aoColumns[iCol].bVisible;
-    oTable.fnSetColumnVis( iCol, false);
-}
