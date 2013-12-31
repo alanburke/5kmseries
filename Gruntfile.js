3290d932470d631c36a6847d5e91ee69db1add82
Alan Burke <alan@cycletraffic.com>
2013-12-31 18:07:53 +0000

    Watch script files

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,6 +70,7 @@ module.exports = function( grunt ) {
           'app/_layouts/**/*',
           'app/_data/**/*',
           'app/css/**/*',
+          'app/scripts/**/*',
         ],
         tasks: ['jekyll']
       },
