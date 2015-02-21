25ce906fc0d8a768edf6832dc30987d158eda9bf
Alan Burke <alan@cycletraffic.com>
2015-02-21 00:00:08 +0000

    Add BrowserSync and ongoing dev functionality

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ var gulp = require('gulp');
     del = require('del');
     shell = require('gulp-shell');
 
+// BrowserSync isn"t a gulp package, and needs to be loaded manually
+var browserSync = require("browser-sync");
+// Need a command for reloading webpages using BrowserSync
+var reload = browserSync.reload;
 
 gulp.task('styles', function() {
   return gulp.src('app/styles/main.scss')
@@ -22,24 +26,44 @@ gulp.task('styles:dev', function() {
     .pipe(sass())
     .pipe(autoprefixer({browsers: ['last 2 versions', 'ie 8', 'ie 9']}))
     .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('dev/css'))
+    // Injects the CSS changes to your browser since Jekyll doesn"t rebuild the CSS
+    .pipe(reload({stream: true}))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task("jekyll", shell.task("jekyll build -s app -d dist"))
-gulp.task("jekyll:dev", shell.task("jekyll build -s app -d dev"))
+gulp.task("jekyll", shell.task("bundle exec jekyll build"))
+gulp.task("jekyll:dev", shell.task("bundle exec jekyll build -s app -d dev"))
+gulp.task("jekyll:rebuild", ["jekyll:dev"], function () {
+  reload;
+});
 
 gulp.task('watch', function() {
 
-  // Create LiveReload server
-  livereload.listen();
-
   // Watch .scss files
-  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/styles/**/*.scss', ['styles:dev']);
 
+  // Watch Jekyll files
+  gulp.watch(['app/**/*.md', 'app/**/*.html', 'app/**/*.yml', 'app/**/*.txt', 'app/**/*.js'], ['jekyll:rebuild']);
+
+  gulp.watch("dev/*.html").on('change', reload);
+
+});
+
+// BrowserSync will serve our site on a local server for us and other devices to use
+// It will also autoreload across all devices as well as keep the viewport synchronized
+// between them.
+gulp.task("serve:dev", ["styles:dev", "jekyll:rebuild"], function () {
+  browserSync({
+    notify: true,
+    open: false,
+    tunnel: false,
+    server: {
+      baseDir: "dev"
+    }
+  });
 });
 
 gulp.task('build', ['jekyll', 'styles']);
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task("default", ["serve:dev", "watch"]);
