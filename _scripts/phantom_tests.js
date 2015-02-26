d5d622fe630ebf31abb9081bc361d4bd7e5c04c7
Alan Burke <alan@cycletraffic.com>
2015-02-26 23:11:15 +0000

    Set Build path

--- _scripts/phantom_tests.js
+++ _scripts/phantom_tests.js
@@ -1,5 +1,5 @@
 casper.test.begin('Home page', 1, function suite(test) {
-  casper.start("./_dist/index.html", function() {
+  casper.start("./dist/index.html", function() {
     test.assertTitle("Galway 5km series", "Galway 5km series title is the one expected");
   });
 
